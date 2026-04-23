e233a358f71e2325c720c9c3991c137d89a367c2
seminar1 <seminar@kolabnow.com>
2026-04-23 19:03:35 +0400

    Added favicon

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,22 @@
 """Корневая конфигурация URL-маршрутов проекта."""
+from urllib.parse import urljoin
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
+# Браузеры часто запрашивают /favicon.ico в корне; редирект на реальный ICO в static.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url=urljoin(settings.STATIC_URL, "favicon.ico"),
+            permanent=False,
+        ),
+    ),
+    path("admin/", admin.site.urls),
     path('accounts/', include('users.urls', namespace='users')),
     path('', include('catalog.urls', namespace='catalog')),
 ]
